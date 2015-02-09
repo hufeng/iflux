e4fdd9b2254d4ad70792b5d55d94bbb7cc145b35
hufeng <hufeng@qianm.com>
2015-02-09 23:25:58 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: './assets/js/app.js',
+    output: {
+	path: './build',
+	filename: 'bundle.js'
+    },
+    module: {
+	loaders: [
+	    {test: /\.js/, loader: 'jsx-loader'}
+	]
+    }
+};
