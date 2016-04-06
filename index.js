9d37ac3c4d846018418ad5071fb790b94b0284bc
hufeng <hufeng@qianmi.com>
2016-04-06 16:19:22 +0800

    add connect-to-store

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = {
   Store: require('./lib/store'),
+  connectToStore: require('./lib/connect-to-store'),
   Validator: require('./lib/validator'),
   msg: require('./lib/msg'),
   mixins: {
-    StoreMixin: require('./lib/mixins/store-mixin')	
-  } 
+    StoreMixin: require('./lib/mixins/store-mixin')
+  }
 };
