54d9e729bf00e27efd1d216b755a1ecb27e9ce39
hufeng <hufeng@qianmi.com>
2019-04-09 17:08:57 +0800

    remove index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Store: require('./lib/store'),
-  connectToStore: require('./lib/connect-to-store'),
-  Validator: require('./lib/validator'),
-  msg: require('./lib/msg'),
-  mixins: {
-    StoreMixin: require('./lib/mixins/store-mixin')
-  }
-};
